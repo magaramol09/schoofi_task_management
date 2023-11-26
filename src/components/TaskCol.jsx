a5a956ceaf8061c226e041f151cdc495500efaf0
Amol Magar <magaramol09@gmail.com>
2023-11-27 01:51:15 +0530

    created the architecture of the project

---
+++ src/components/TaskCol.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class TaskCol extends Component {
+  render() {
+    return (
+      <div>TaskCol</div>
+    )
+  }
+}
+
+export default TaskCol
---
+++ src/components/TaskDetailsView.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class TaskDetailsView extends Component {
+  render() {
+    return (
+      <div>TaskDetailsView</div>
+    )
+  }
+}
+
+export default TaskDetailsView
