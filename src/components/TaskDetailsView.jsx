9981bd85d759a359913d729f6be76a359aee5966
Amol Magar <magaramol09@gmail.com>
2023-11-27 02:15:05 +0530

    added task view modal

--- src/components/TaskDetailsView.jsx
+++ src/components/TaskDetailsView.jsx
@@ -1,11 +1,69 @@
-import React, { Component } from 'react'
+import { useEffect, useRef } from 'react';
+import { Modal, Form, Input } from 'antd';
 
-export class TaskDetailsView extends Component {
-  render() {
-    return (
-      <div>TaskDetailsView</div>
-    )
-  }
+function TaskboardItemFormModal({ visible, initialValues, onCancel, onOk }) {
+  const [form] = Form.useForm();
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (visible) {
+      // Focus on the first input when the modal is opened
+      inputRef.current?.focus();
+      form.resetFields();
+    }
+  }, [form, visible]);
+
+  return (
+    <Modal
+      title="Add Item"
+      visible={visible}
+      destroyOnClose
+      // To make dynamically changing initialValues work with Form
+      forceRender
+      onCancel={onCancel}
+      onOk={() => form.submit()}
+    >
+      <Form
+        autoComplete="off"
+        form={form}
+        layout="vertical"
+        initialValues={initialValues}
+        onFinish={(values) => {
+          onOk(values);
+          form.resetFields();
+          onCancel();
+        }}
+      >
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[
+            { required: true, message: "'Title' is required" },
+            {
+              max: 100,
+              message: "'Title' can not be longer than 100 characters",
+            },
+          ]}
+        >
+          <Input ref={inputRef} autoFocus />
+        </Form.Item>
+        <Form.Item
+          name="description"
+          label="Description"
+          rules={[
+            { required: true, message: "'Description' is required" },
+            {
+              max: 400,
+              message: "'Description' can not be longer than 400 characters",
+            },
+          ]}
+        >
+          <Input.TextArea rows={4} />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 }
 
-export default TaskDetailsView
+export default TaskboardItemFormModal;
